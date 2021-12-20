20143bd91fe078aa056c7161d47a6dff79bab075
LikeX86 <pedroemailcomercial@gmail.com>
2021-12-20 19:40:23 -0300

    just a commit

--- Loja/settings.py
+++ Loja/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m%v4u8gnsepe1a3wib=(7$u@p0bay48=+oejvh5a^h3s@6c7im'
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+# SEGREDINHO DEBUG E SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', False)
 
 ALLOWED_HOSTS = []
 
